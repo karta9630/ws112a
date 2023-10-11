bf777bc9c272d95730635971121d75287b4be05e
karta9630 <4909044@gmail.com>
2023-10-11 20:06:43 +0800

    1

--- homework/4/1.js
+++ homework/4/1.js
@@ -1,10 +1,7 @@
 import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
  
 const peoples = new Map();
-peoples.set("john", {
-  name: "john",
-  tel: "082-313345",
-});
+
 peoples.set("mary", {
   name: "mary",
   tel: "082-313543",
@@ -18,25 +15,52 @@ router
   .get("/people", (ctx) => {
     ctx.response.body = Array.from(peoples.values());
   })
-  .get("/people/add", (ctx) => {
-    let params = ctx.request.url.searchParams
-    let name = params.get('name')
-    let tel = params.get('tel')
-    console.log(`name=${name} tel=${tel}`)
-    if (peoples.get(name)) {
-      ctx.response.body = {'error':`name=${name} 已經存在！`}
-    } else {
-      peoples.set(name, {name, tel})
+  .post("/people/add", async (ctx) => {
+    const body = ctx.request.body()
+    if (body.type === "form") {
+      const pairs = await body.value
+      console.log('pairs=', pairs)
+      const params = {}
+      for (const [key, value] of pairs) {
+        params[key] = value
+      }
+      console.log('params=', params)
+      let name = params['name']
+      let tel = params['tel']
       ctx.response.type = 'text/html'
-      ctx.response.body = `<p>新增 (${name}, ${tel}) 成功</p><p><a href="/people/">列出所有人員</a></p>`
+      console.log(`name=${name} tel=${tel}`)
+      if (peoples.get(name)) {
+        ctx.response.body = `<p>帳號重複</p><p><a href="/public/">通訊管理系統</a></p>`       
+                
+      } else {
+        peoples.set(name, {name, tel})
+        ctx.response.body = `<p>新增 (${name}, ${tel})註冊成功</p></p><p><a href="/public/">通訊管理系統</a></p>`
+      }
+  
     }
+
   })
-  .get("/people/find", (ctx) => {
-    let params = ctx.request.url.searchParams    
-    let name = params.get('name')
-    console.log('name=', name)
-    if (peoples.has(name)) {
-      ctx.response.body = peoples.get(name);
+  .post("/people/find", async (ctx) => {
+    const body = ctx.request.body()
+    if (body.type === "form") {
+      const pairs = await body.value
+      console.log('pairs=', pairs)
+      const params = {}
+      for (const [key, value] of pairs) {
+        params[key] = value
+      }
+      console.log('params=', params)
+      let name = params['name']
+      let tel = params['tel']
+      console.log(`name=${name} tel=${tel}`)
+      if (peoples.get(name)) {
+        ctx.response.body = ({'error':`name=${name} 帳號已被別人使用過 `})        
+      } else {
+        peoples.set(name, {name, tel})
+        ctx.response.type = 'text/html'
+        ctx.response.body = `<p>新增 (${name}, ${tel})註冊成功</p></p><p><a href="/public/">通訊管理系統</a></p>`
+      }
+  
     }
   })
   .get("/public/(.*)", async (ctx) => {
@@ -53,6 +77,6 @@ const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log('start at : http://127.0.0.1:8001')
+console.log('start at : http://127.0.0.1:8000/public/')
 
-await app.listen({ port: 8001 });
+await app.listen({ port: 8000 });
