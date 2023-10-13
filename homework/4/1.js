1e01725dda143f3270f4b91d0ada5be1aaab95b0
karta9630 <4909044@gmail.com>
2023-10-13 11:23:16 +0800

    1

--- homework/4/1.js
+++ homework/4/1.js
@@ -76,6 +76,6 @@ const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log('start at : http://127.0.0.1:8000/public/')
+console.log('start at : http://127.0.0.1:8002/public/')
 
-await app.listen({ port: 8000 });
+await app.listen({ port: 8002 });
